4d856d68cdaf262781e0e299af0f10de952e95df
stsTom <tamilaustsiapan@gmail.com>
2026-03-10 14:07:58 +0100

    Implemented readline logic

--- src/repl.js
+++ src/repl.js
@@ -1,7 +1,9 @@
 import readline from "node:readline/promises"
 import { stdin, stdout } from "node:process"
 
-export const repl = async (directory) => {
+export const repl = async (homeDirectory) => {
+  var directory = homeDirectory
+  
   const rl = readline.createInterface({
     input: stdin,
     output: stdout,
@@ -10,4 +12,17 @@ export const repl = async (directory) => {
 
   console.log('Welcome to Data Processing CLI!')
   console.log('You are currently in ', directory)
+  rl.prompt()
+
+  rl.on('line', (line) => {
+    const input = line.trim()
+
+    switch(input){
+
+    }
+
+    console.log('You are currently in ', directory)
+  })
+
+  rl.on('close', () => console.log('Thank you for using Data Processing CLI!'))
 }
