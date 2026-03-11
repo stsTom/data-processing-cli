b2bc08d4bc8c52735692f826c88af4555d7f459c
stsTom <tamilaustsiapan@gmail.com>
2026-03-11 11:24:56 +0100

    Implemented basic encryption logic

--- src/commands/encrypt.js
+++ src/commands/encrypt.js
@@ -1,16 +1,42 @@
-import { parseArguments } from "../../utils/argParser"
-import { resolvePath } from "../../utils/pathResolver"
+import { finished, pipeline } from "node:stream/promises"
+import { parseArguments } from "../../utils/argParser.js"
+import { resolvePath } from "../../utils/pathResolver.js"
 import fs from 'fs/promises'
+import crypto from 'node:crypto'
 
 export const encrypt = async () => {
   const args = await parseArguments()
-  
+  //need to handle a situation with no input
+  //need to accept arguments (argParser?)
+
   try{
     await fs.access(await resolvePath(args['--input']))
-  }catch{
-    console.log('no access to the input')
-    throw 'Operation failed'
-  }
 
+    const salt = crypto.randomBytes(16)
+    const iv = crypto.randomBytes(12)
+    const key = crypto.scrypt(args['--password'], salt)
+    const cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
+
+    const streamSource = await fs.createReadStream(args['--input'])
+    const streamOutput = await fs.createWriteStream(args['--output'])
   
+    streamOutput.write(salt)
+    streamOutput.write(iv)
+
+    await pipeline(streamSource, cipher, streamOutput, {end: false})
+    cipher.end()
+    await finished(cipher)
+    
+    const authTag = cipher.getAuthTag()
+
+    streamOutput.write(authTag)
+    streamOutput.end()
+    await finished(streamOutput)
+  }catch (err){
+    console.log(err)
+    console.log('Operation failed')
+    // return
+  }finally{
+    console.log('finally')
+  }
 }
--- src/repl.js
+++ src/repl.js
@@ -1,5 +1,6 @@
 import readline from "node:readline/promises"
 import { stdin, stdout } from "node:process"
+import { encrypt } from "./commands/encrypt.js"
 
 export const repl = async (homeDirectory) => {
   var directory = homeDirectory
@@ -20,13 +21,18 @@ export const repl = async (homeDirectory) => {
     // this try catch block need to be tested when some of the commands will be implemented
     try{
       switch(input){
+        case 'encrypt':
+          encrypt()
+          break
         case '.exit':
           rl.close()
           return
         default: 
           console.log('Invalid input')
+          break
       }
     }catch{
+      console.log('switch stopped')
       console.log('Operation failed')
     }
 
