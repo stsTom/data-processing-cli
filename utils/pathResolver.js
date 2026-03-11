2e012c142d3ff44f94631ca5f572790eae428977
stsTom <tamilaustsiapan@gmail.com>
2026-03-11 09:11:15 +0100

    Fixed pathResolver.js logic

--- utils/pathResolver.js
+++ utils/pathResolver.js
@@ -1,7 +1,7 @@
 import { parseArguments } from "./argParser"
 import path from 'path'
 
-export const resolvePath = async (pathPointed, currentDirectory) => {
-  const relativePath = path.relative(currentDirectory, pathPointed)
-  return relativePath
+export const resolvePath = async (pathPointed) => {
+  const absolutePath = path.resolve(pathPointed)
+  return absolutePath
 }
