640b3d3373ba46fc543f2ed806c12fee8355d51d
stsTom <tamilaustsiapan@gmail.com>
2026-03-13 20:15:19 +0100

    Wrapped worker creation in a promise

--- src/commands/logStats.js
+++ src/commands/logStats.js
@@ -2,7 +2,7 @@ import { parseArguments } from "../../utils/argParser.js"
 import { resolvePath } from "../../utils/pathResolver.js"
 import os from 'os'
 import fs from 'fs/promises'
-import { Worker } from "worker_threads"
+import { Worker, workerData } from "worker_threads"
 
 export const readStats = async (commandLine, currentDir) => {
   const chunksCount = os.availableParallelism()
@@ -16,6 +16,7 @@ export const readStats = async (commandLine, currentDir) => {
 
     const buffer = Buffer.alloc(1)
     const chunkSize = (await fs.stat(inputPath)).size / chunksCount
+    const allWorkersData = []
 
     var startPosition = 0
     var endPosition = chunkSize
@@ -29,20 +30,48 @@ export const readStats = async (commandLine, currentDir) => {
       }else{
         while (true) {
           fileHandle.read(buffer, 0, 1, endPosition)
+
           if (buffer[0] === 10){
             //create new Worker, send it start position and end position
             break;
           }
+
           endPosition++
         }
       }
-      const logWorker = new Worker("../../workers/logWorker.js", { start: startPosition, end: endPosition, inputPath })
+
+      const workerPromise = new Promise((resolve, reject) => {
+        const logWorker = new Worker(
+          "../../workers/logWorker.js",
+          { workerData:{
+            start: startPosition,
+            end: endPosition,
+            inputPath
+          }
+        })
+
+        logWorker.on('message', (result) => {
+          resolve(result); 
+        });
+
+        logWorker.on('error', (err) => {
+          reject(err);
+        });
+      })
+
+      allWorkersData.push[workerPromise]
       
       startPosition = ++endPosition
       endPosition += chunkSize
       /* if it works at all, end position will be increased even after the last worker would be created
       it shouldn't affect anything but it's an unneccessary calculation */
     }
+
+    const finalResults = await Promise.all(allWorkersData)
+
+    await fileHandle.close()
+
+    //merge all data into a final file
   }catch(err){
     console.log(err)
     console.log('Operation failed')
